9d16bad063b9cec5c34c2970749c91c9b890de60
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-05-19 23:43:24 +0200

    adding allocate and swap methods in memory class

---
+++ src/main/java/interpreter/OSApp.java
@@ -0,0 +1,9 @@
+package interpreter;
+
+public class OSApp {
+
+
+    public void init(){
+        //TODO: intianlize the mutex
+    }
+}
--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -5,8 +5,32 @@ import constants.Constants;
 
 public class Memory {
     private Word[] memory;
+    private int start;
+    private int end;
+    private int nItems;
 
     public Memory(){
         memory = new Word[Constants.MEMORY_SIZE];
+        start = -1;
+        end = 0;
+    }
+
+    public void allocate(){
+        //TODO: implement the method
+    }
+
+    public void swap(){
+        //TODO: implement the method
+    }
+
+    public boolean isEmpty() {
+        return nItems == 0;
+    }
+
+    public boolean isFull() {
+        return (nItems == Constants.MEMORY_SIZE);
+    }
+    public int size(){
+        return nItems;
     }
 }
