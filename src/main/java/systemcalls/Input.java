3dcb4f1c1098c365419921468380a84836ba8a24
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-05-20 23:15:30 +0300

    add system-calls

--- src/main/java/systemcalls/Input.java
+++ src/main/java/systemcalls/Input.java
@@ -1,4 +1,13 @@
 package systemcalls;
 
+import java.util.Scanner;
+
 public class Input {
+	
+	private Scanner sc;
+
+	public String readInput() {
+		sc = new Scanner(System.in);
+		return sc.nextLine();
+	}
 }
--- src/main/java/systemcalls/WriteFile.java
+++ src/main/java/systemcalls/WriteFile.java
@@ -1,4 +1,19 @@
 package systemcalls;
 
+import java.io.*;
+import java.util.*;
+
 public class WriteFile {
+	private PrintWriter pw;
+
+	public void writeFile(ArrayList<String> fileLines, String filePath) throws IOException {
+		pw = new PrintWriter(new FileWriter(filePath));
+
+		for (String line : fileLines) {
+			pw.println(line);
+		}
+		pw.flush();
+		pw.close();
+	}
+
 }
