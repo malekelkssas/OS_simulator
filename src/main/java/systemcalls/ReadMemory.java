bf2e18fd282ff1c065efe0f24930dbddccc9d979
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-05-25 01:19:19 +0300

    add read and write memory

--- src/main/java/systemcalls/ReadMemory.java
+++ src/main/java/systemcalls/ReadMemory.java
@@ -1,4 +1,12 @@
 package systemcalls;
 
+import exceptions.NoSuchProcessException;
+import memory.Memory;
+import storage.Process;
+
 public class ReadMemory {
+
+	public static Process readProcess(int id) throws NoSuchProcessException {
+		return Memory.getInstance().getProcess(id);
+	}
 }
