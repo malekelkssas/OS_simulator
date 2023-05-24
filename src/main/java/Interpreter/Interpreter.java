1933c8bb821b2d44e563da78895570af78f685a8
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-05-24 15:53:39 +0300

    Update Interpreter.java

--- src/main/java/Interpreter/Interpreter.java
+++ src/main/java/Interpreter/Interpreter.java
@@ -1,28 +1,31 @@
 package Interpreter;
+
 import memory.Memory;
 import mutexes.Mutex;
-import storage.*;
+
 public class Interpreter {
-	private Interpreter instance; 
-	private Memory memory;
-	private Interpreter() {
-		instance = new Interpreter();
-		Memory memeory = new Memory();
-		Mutex metux = Mutex.getInstance();
-	}
+    private Interpreter instance;
+    private Memory memory;
 
-	public Interpreter getInstance() {
-		if (instance == null) {
-			instance = new Interpreter();
-		}
-		return instance;
-	}
-	
-	public static void read(String filePath) {
-		
-	}
-	
+    private Interpreter() {
+        instance = new Interpreter();
+        Memory memeory = new Memory();
+        Mutex metux = Mutex.getInstance();
+    }
 
-	
+    public Interpreter getInstance() {
+        if (instance == null) {
+            instance = new Interpreter();
+        }
+        return instance;
+    }
+
+    public static void read(String filePath) {
+
+    }
+
+    public Memory getMemory(){
+        return memory;
+    }
 
 }
