1af54ffd08754aaa4f7243708653611a5b6dabe2
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-05-24 15:26:54 +0300

    unndeed

--- src/main/java/interpreter/Interpreter.java
+++ src/main/java/interpreter/Interpreter.java
@@ -1,31 +1,28 @@
 package Interpreter;
-
 import memory.Memory;
 import mutexes.Mutex;
-
+import storage.*;
 public class Interpreter {
-    private Interpreter instance;
-    private Memory memory;
+	private Interpreter instance; 
+	private Memory memory;
+	private Interpreter() {
+		instance = new Interpreter();
+		Memory memeory = new Memory();
+		Mutex metux = Mutex.getInstance();
+	}
 
-    private Interpreter() {
-        instance = new Interpreter();
-        Memory memeory = new Memory();
-        Mutex metux = Mutex.getInstance();
-    }
+	public Interpreter getInstance() {
+		if (instance == null) {
+			instance = new Interpreter();
+		}
+		return instance;
+	}
+	
+	public static void read(String filePath) {
+		
+	}
+	
 
-    public Interpreter getInstance() {
-        if (instance == null) {
-            instance = new Interpreter();
-        }
-        return instance;
-    }
-
-    public static void read(String filePath) {
-
-    }
-
-    public Memory getMemory(){
-        return memory;
-    }
+	
 
 }
